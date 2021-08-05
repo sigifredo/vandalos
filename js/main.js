a12a88bc71ebb00f5bfb7cda56c6ff2bf5136e86
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2021-08-05 07:55:26 -0500

    Se crea un archivo de recursos a parte del archivo js principal.

--- js/main.js
+++ js/main.js
@@ -1,45 +1,10 @@
 'use strict';
 
+import { resources } from "./resources.js";
 import * as THREE from '/build/three.module.js';
 
 import { FirstPersonControls } from '/build/jsm/controls/FirstPersonControls.js';
 import { CSS3DRenderer, CSS3DObject } from '/build/jsm/renderers/CSS3DRenderer.js';
-
-const resources = [
-    { type: 'img', path: 'assets/vandalo-0000.jpg' },
-    { type: 'img', path: 'assets/vandalo-0001.jpg' },
-    { type: 'img', path: 'assets/vandalo-0002.jpg' },
-    { type: 'img', path: 'assets/vandalo-0003.jpg' },
-    { type: 'img', path: 'assets/vandalo-0004.jpg' },
-    { type: 'img', path: 'assets/vandalo-0005.jpg' },
-    { type: 'img', path: 'assets/vandalo-0006.jpg' },
-    { type: 'img', path: 'assets/vandalo-0007.jpg' },
-    { type: 'img', path: 'assets/vandalo-0008.jpg' },
-    { type: 'img', path: 'assets/vandalo-0009.jpg' },
-    { type: 'img', path: 'assets/vandalo-0010.jpg' },
-    { type: 'img', path: 'assets/vandalo-0011.jpg' },
-    { type: 'img', path: 'assets/vandalo-0012.jpg' },
-    { type: 'img', path: 'assets/vandalo-0013.jpg' },
-    { type: 'img', path: 'assets/vandalo-0014.jpg' },
-    { type: 'img', path: 'assets/vandalo-0015.jpg' },
-    { type: 'img', path: 'assets/vandalo-0016.jpg' },
-    { type: 'img', path: 'assets/vandalo-0017.jpg' },
-    { type: 'img', path: 'assets/vandalo-0018.jpg' },
-    { type: 'img', path: 'assets/vandalo-0019.jpg' },
-    { type: 'img', path: 'assets/vandalo-0020.jpg' },
-    { type: 'img', path: 'assets/vandalo-0021.jpg' },
-    { type: 'img', path: 'assets/vandalo-0022.jpg' },
-    { type: 'img', path: 'assets/vandalo-0023.jpg' },
-    { type: 'img', path: 'assets/vandalo-0024.jpg' },
-    { type: 'img', path: 'assets/vandalo-0025.jpg' },
-    { type: 'img', path: 'assets/vandalo-0026.jpg' },
-    { type: 'img', path: 'assets/vandalo-0027.jpg' },
-    { type: 'img', path: 'assets/vandalo-0028.jpg' },
-    { type: 'img', path: 'assets/vandalo-0029.jpg' },
-    { type: 'yt', path: 'K9_VFxzCuQ0' },
-    { type: 'yt', path: '9pdj4iJD08s' },
-    { type: 'yt', path: 'bwmSjveL3Lc' },
-];
 
 let camera, scene, rendererGl, rendererCSS;
 let controls;
