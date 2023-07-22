7b1ec21659e437b913572383cceb410b24f59d2b
Sigifredo Escobar Gómez <sigifredo89@gmail.com>
2023-07-22 13:35:16 -0500

    DirectionalLightHelper added.

--- js/Lights.js
+++ js/Lights.js
@@ -4,10 +4,14 @@ export class Lights {
     constructor(env) {
         const ambient = new THREE.AmbientLight(0xb9d5ff, 0.5);
         const directional = new THREE.DirectionalLight(0xb9d5ff, 0.5);
-        directional.position.set(4, 5, -2);
+        directional.position.set(2, 2, -1);
 
-        env.scene.add(ambient);
-        env.scene.add(directional);
+        env.scene.add(ambient, directional);
+
+        if (env.gui) {
+            const directionalHelper = new THREE.DirectionalLightHelper(directional, 0.2);
+            env.scene.add(directionalHelper);
+        }
     }
 }
 
